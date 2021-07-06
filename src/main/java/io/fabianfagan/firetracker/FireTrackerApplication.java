4a6b61b177fadef0d793b88ce2c506b8bc73bd5e
Fabian <fabian.fagan99@gmail.com>
2021-07-06 16:27:38 +1200

    first commit

---
+++ src/main/java/io/fabianfagan/firetracker/FireTrackerApplication.java
@@ -0,0 +1,13 @@
+package io.fabianfagan.firetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FireTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FireTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/fabianfagan/firetracker/FireTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.fabianfagan.firetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FireTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
